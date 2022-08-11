67e5c4484596aac6f3ee06e8191b85cbf2b07dd9
devops <110580465+Lori-devops@users.noreply.github.com>
2022-08-11 21:18:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World- at 19:46 -triggered by webhook");
+    System.out.println("Hello World- at 19:48 -triggered by webhook");
     }
 }
